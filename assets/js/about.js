430c7b9d910f5a2e32be9d3266b7b6f60cd3c1d4
groms12 <gms1204@mail.ru>
2021-01-29 17:27:15 +0300

    work with slider

--- assets/js/about.js
+++ assets/js/about.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
 
     const swiper = new Swiper('.swiper-container', {
         loop: true,
+        // centeredSlides: true,
         slidesPerView: 4,
-        spacebetween: 35,
+        spacebetween: 40,
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
