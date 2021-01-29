384115828e194374b8c3bb5239aee0191ec1840c
groms12 <gms1204@mail.ru>
2021-01-29 17:02:01 +0300

    create 4 block for desktop

---
+++ assets/js/about.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 4,
+        spacebetween: 35,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+      });
+
+});
