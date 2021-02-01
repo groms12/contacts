206727737900b47074df264c41e7ae5e03ac362e
groms12 <gms1204@mail.ru>
2021-02-01 11:40:39 +0300

    add media for last block

--- assets/js/about.js
+++ assets/js/about.js
@@ -8,7 +8,31 @@ $(document).ready(function () {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
-
+        breakpoints: {
+          768: {
+            slidesPerView: 2,
+            spacebetween: 40
+          },
+          885: {
+            slidesPerView: 3,
+            spacebetween: 40
+          },
+          1100: {
+            slidesPerView: 4,
+            spacebetween: 40
+          }
+            
+        }
       });
+
+    $('.btn').click(function () { 
+
+      $('.about__media').append(
+        '<img src="/assets/img/certificates/img4.png" alt="" class="about__media__img">'+
+        '<img src="/assets/img/certificates/img1.png" alt="" class="about__media__img">'+
+				// '<img src="/assets/img/certificates/img2.png" alt="" class="about__media__img">'+
+				'<img src="/assets/img/certificates/img3.png" alt="" class="about__media__img">'
+      )
+    })
 
 });
