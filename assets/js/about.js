9225a315da201d350d45f19283d4a8482e16e9b9
groms12 <gms1204@mail.ru>
2021-02-03 23:41:47 +0300

    add colorbox for about certificates

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  $("a.gallery").colorbox({
+    rel: 'gallery',
+    maxHeight: "90%",
+    current: "Изображение {current} из {total}",
+    opacity: 0.6
+  });
 
     const swiper = new Swiper('.swiper-container', {
         loop: true,
@@ -28,11 +34,19 @@ $(document).ready(function () {
     $('.btn').click(function () { 
 
       $('.about__media').append(
-        '<img src="/assets/img/certificates/img4.png" alt="" class="about__media__img">'+
-        '<img src="/assets/img/certificates/img1.png" alt="" class="about__media__img">'+
-				// '<img src="/assets/img/certificates/img2.png" alt="" class="about__media__img">'+
-				'<img src="/assets/img/certificates/img3.png" alt="" class="about__media__img">'
+        // '<img src="/assets/img/certificates/img4.png" alt="" class="about__media__img">'+
+        // '<img src="/assets/img/certificates/img1.png" alt="" class="about__media__img">'+
+        // '<img src="/assets/img/certificates/img3.png" alt="" class="about__media__img">'+
+        '<a href="assets/img/certificates/img2.png" class="about__media__img gallery"><img src="assets/img/certificates/img2.png" alt=""></a>'+
+        '<a href="assets/img/certificates/img4.png" class="about__media__img gallery"><img src="assets/img/certificates/img4.png" alt=""></a>'+
+        '<a href="assets/img/certificates/img3.png" class="about__media__img gallery"><img src="assets/img/certificates/img3.png" alt=""></a>'
       )
+      $("a.gallery").colorbox({
+        rel: 'gallery',
+        maxHeight: "90%",
+        current: "Изображение {current} из {total}",
+        opacity: 0.6
+      });
     })
 
 });
