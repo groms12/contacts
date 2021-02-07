8ed06d77310784f10a4bc74717dd1b9b737ffb20
groms12 <gms1204@mail.ru>
2021-02-07 22:23:02 +0300

    add media for first block

--- assets/js/card-objects.js
+++ assets/js/card-objects.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('.card__objects__images__small img').click(function() {
         var srcLittle = this.src;
-        var srcBig = $('.card__objects__images__big').src;
+        var srcBig = $('.card__objects__images__big').attr('src');
         $('.card__objects__images__big').attr('src', srcLittle);
-        $('.card__objects__images__small img').attr('src', srcBig);
+        $(this).attr('src', srcBig);
     })
 });
