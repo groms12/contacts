ccd6ecc5a0481d8103c9d7cc6afa92317842f8db
groms12 <gms1204@mail.ru>
2021-02-17 01:02:52 +0300

    add new html for gallery

--- assets/js/card-objects.js
+++ assets/js/card-objects.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-    $('.card__objects__images__small img').click(function() {
-        var srcLittle = this.src;
+    $('.card__objects__images__small a').click(function() {
+        var srcLittle = $(this).attr('data-img');
         // var srcBig = $('.card__objects__images__big').attr('src');
+        $('.card__objects__images__small a').removeClass('active');
+        $(this).addClass('active');
         $('.card__objects__images__big').attr('src', srcLittle);
         $('.card__objects__images__big').attr('width', '680');
         $(this).attr('src', srcBig);
--- assets/js/product-card.js
+++ assets/js/product-card.js
@@ -46,10 +46,12 @@ $(document).ready(function () {
     });
     
 
-    $('.product-card__object__images__small div img').click(function() {
-        var srcLittle = this.src;
+    $('.product-card__object__images__small a').click(function() {
+        var srcLittle = $(this).attr('data-img');
+        $('.product-card__object__images__small a').removeClass('active');
+        $(this).addClass('active');
         var srcBig = $('.product-card__object__images__big').src;
         $('.product-card__object__images__big').attr('src', srcLittle);
-        $('.product-card__object__images__small div img').attr('src', srcBig);
+        $('.product-card__object__images__small a img').attr('src', srcBig);
     })
 });
