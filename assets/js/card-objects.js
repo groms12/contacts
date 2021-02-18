bc8bc3c453a6f7bf09b0236f087df6780cac501e
groms12 <gms1204@mail.ru>
2021-02-19 00:27:27 +0300

    work with gallery and margin

--- assets/js/card-objects.js
+++ assets/js/card-objects.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $('.card__gallery__thumbs a').click(function() {
         var srcLittle = $(this).attr('data-img');
         // var srcBig = $('.card__objects__images__big').attr('src');
-        $('.card__gallery__thumbs a').removeClass('active');
-        $(this).addClass('active');
+        $('.card__gallery__thumbs a img').removeClass('active');
+        // $(this).addClass('active');
         $('.card__gallery__big').attr('src', srcLittle);
         $('.card__gallery__big').attr('width', '680');
         $(this).attr('src', srcBig);
