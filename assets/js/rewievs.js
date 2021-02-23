7c920404b9bce80d7090cfa8858b079fb7046bf8
groms12 <gms1204@mail.ru>
2021-02-23 21:45:17 +0300

    add desлещз version for rewievs

---
+++ assets/js/rewievs.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 2,
+        spaceBetween: 45,
+        navigation: {
+          nextEl: '.swiper-button-next1',
+          prevEl: '.swiper-button-prev1',
+        },
+
+      });
+
+});
