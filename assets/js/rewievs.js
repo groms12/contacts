9da3532175c4958348f14cff90a9a9c8b1234e0b
groms12 <gms1204@mail.ru>
2021-02-23 23:55:15 +0300

    add tablet verdion

--- assets/js/rewievs.js
+++ assets/js/rewievs.js
@@ -2,12 +2,31 @@ $(document).ready(function () {
 
     const swiper = new Swiper('.swiper-container', {
         slidesPerView: 2,
-        spaceBetween: 45,
+        // spaceBetween: 45,
         navigation: {
           nextEl: '.swiper-button-next1',
           prevEl: '.swiper-button-prev1',
         },
-
+        breakpoints: {
+            500: {
+                slidesPerView: 1,
+            },
+            851: {
+                slidesPerView: 2,
+            },
+            965: {
+                spaceBetween: 10,
+            },
+            1020: {
+                spaceBetween: 20, 
+            },
+            1110: {
+                spaceBetween: 30, 
+            },
+            1170: {
+                spaceBetween: 45,
+            }
+          }
       });
 
 });
