72ad22cbaa7a7d6ed10123e911a68c7d5202a86a
groms12 <gms1204@mail.ru>
2021-02-24 12:19:20 +0300

    add mobile version for rewievs

--- assets/js/rewievs.js
+++ assets/js/rewievs.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-    const swiper = new Swiper('.swiper-container', {
+
+    if ($(window).width() > 565){
+       const swiper = new Swiper('.swiper-container', {
         slidesPerView: 2,
         // spaceBetween: 45,
         navigation: {
@@ -8,8 +10,9 @@ $(document).ready(function () {
           prevEl: '.swiper-button-prev1',
         },
         breakpoints: {
-            500: {
+            565: {
                 slidesPerView: 1,
+                direction: 'horizontal',
             },
             851: {
                 slidesPerView: 2,
@@ -27,6 +30,25 @@ $(document).ready(function () {
                 spaceBetween: 45,
             }
           }
-      });
+      }); 
+    }
+    
+    $('.btn').click(function() {
+        $('.rewievs__slider-wrapper').append(
+            '<div class="swiper-slide rewievs__slider__slide">'+
+            '<div class="rewievs__slider__slide__people">'+
+                '<img src="assets/img/rewievs/people.png" alt="" class="rewievs__slider__slide__people__img">'+
+                '<div class="rewievs__slider__slide__people__text">'+
+                    '<div class="rewievs__slider__slide__people__text__name">Марина</div>'+
+                    '<div class="rewievs__slider__slide__people__text__city">Москва</div>'+
+                '</div>'+
+            '</div>'+
+            '<div class="rewievs__slider__slide__description">'+
+                '<p class="rewievs__slider__slide__description__text">Очень понравилась работа сотрудников — не просто сделали и забыли, а комплексно подошли к задаче. Сделали проект, купил емкость у вас же, монтаж и пусконаладку, заправку емкости газом.</p>'+
+                '<a href="#" class="rewievs__slider__slide__description__more">Читать полностью</a>'+
+            '</div>'+
+        '</div>'
+        )
+    });
 
 });
